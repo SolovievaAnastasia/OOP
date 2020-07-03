a65ba2c808cdd7664fa238c371333cc6e46fe1e0
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2020-07-04 02:51:13 +0300

    Update and rename th7.cpp to theme7.cpp

--- theme7.cpp
+++ theme7.cpp
@@ -5,10 +5,8 @@ class Ship {
  private:
   string name;
   int id;
-
  public:
   Ship(string name, int id) : p_name(name), p_id(id) {}
-
   string getName() { return p_name; }
   int getID() { return p_id; }
 };
@@ -16,10 +14,8 @@ class Machine {
  private:
   string p_engine;
   int p_kpd;
-
  public:
   Machine(string engine, int kpd) : p_engine(engine), p_kpd(kpd) {}
-
   string getEngine() { return p_engine; }
   double getKpd() { return p_kpd; }
 };
@@ -27,7 +23,6 @@ class Machine {
 class Parohod : public Ship, public Machine {
  private:
   int p_year;
-
  public:
   Parohod(string name, int id, string engine, int kpd, int year)
       : Ship(name, id), Machine(engine, kpd), p_year(year) {}
