36f660a6515f411560cd92971957b984e55fdaa3
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2020-07-03 10:48:12 +0300

    Create th8.cpp

---
+++ th8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class WoodTh {
+ public:
+  virtual void Make() = 0;
+};
+
+class Ship : public WoodTh {
+ public:
+  void Make() { cout << "Ship" << endl; }
+};
+
+class Chair : public WoodTh {
+ public:
+  void Make() { cout << "Chair" << endl; }
+};
+
+int main() {
+  Ship A;
+  Chair B;
+
+  A.Draw();
+  B.Draw();
+
+  return 0;
+}
