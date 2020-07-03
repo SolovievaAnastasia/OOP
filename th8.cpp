b1c2b4b69f5bd2b916add23b99f204bc5b3d1bed
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2020-07-04 02:51:49 +0300

    Update th8.cpp

--- th8.cpp
+++ th8.cpp
@@ -5,12 +5,10 @@ class WoodTh {
  public:
   virtual void Make() = 0;
 };
-
 class Ship : public WoodTh {
  public:
   void Make() { cout << "Ship" << endl; }
 };
-
 class Chair : public WoodTh {
  public:
   void Make() { cout << "Chair" << endl; }
@@ -19,9 +17,7 @@ class Chair : public WoodTh {
 int main() {
   Ship A;
   Chair B;
-
   A.Make();
   B.Make();
-
   return 0;
 }
